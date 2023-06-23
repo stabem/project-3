d60a2912a995b06ad0fe0473710ccc6a921a8f85
stabem <gabrielstabelini22@gmail.com>
2023-06-23 13:01:29 -0300

    inicial

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "no-unused-vars": "off"
+  },
+};
---
+++ src/templates/App/index.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default Home;
